14e593ed2c9a81507937a426226adcd5f8106d97
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-04 15:15:44 +0200

    Update pyt.m

--- pyt.m
+++ pyt.m
@@ -1,12 +1,22 @@
-% matlab_script.m
+
 clear all;
 
-% Podanie dwóch liczb całkowitych
-a = 10;
-b = 5;
-py.importlib.import_module('sum_numbers');
-% Wywołanie Pythona i obliczenie sumy
-result = py.sum_numbers.sum_numbers(a, b);
+%a = 10;
+%b = 5;
+%py.importlib.import_module('sum_numbers');
 
-% Wyświetlenie wyniku
-fprintf('Suma liczb %d i %d wynosi: %d\n', a, b, result);
+%result = py.sum_numbers.sum_numbers(a, b);
+
+%fprintf('Suma liczb %d i %d wynosi: %d\n', a, b, result);
+
+n = 5;
+
+means = [1.0, 1.5, 2.0, 0.5, 1.2];
+
+action = 2;
+
+reward = MultiArmedBandit(action, means);
+
+disp(['Wybrana akcja: ', num2str(action)]);
+disp(['Otrzymana nagroda: ', num2str(reward)]);
+
