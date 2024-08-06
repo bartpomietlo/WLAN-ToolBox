1f989f05cec2653ad5afdeb7a8d8c808b13b4d97
Bartłomiej Pomietło <163325596+bartpomietlo@users.noreply.github.com>
2024-08-06 12:30:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import os
 
 eng = matlab.engine.start_matlab()
 
-matlab_script_path = os.path.abspath('pyt.m')
-
+matlab_script_path = os.path.abspath('MultiArmedBandit.m')
 eng.addpath(os.path.dirname(matlab_script_path))
 
-eng.run(matlab_script_path, nargout=0)
+n = 10
+means = matlab.double([1.0, 1.5, 2.0, 0.5, 1.2])
+action = 2
+
+reward = eng.MultiArmedBandit(action, means)
+
+print(f"Wybrana akcja: {action}")
+print(f"Otrzymana nagroda: {reward}")
 
 eng.quit()
